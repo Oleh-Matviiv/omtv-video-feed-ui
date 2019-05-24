b02da2cd3f21f2e24bbd512847098c714c3b4477
Oleh Matviiv <oleh.matviiv@gmail.com>
2019-05-24 12:16:26 +0300

    master - properly format number of views

--- src/ui/app/video/Views.js
+++ src/ui/app/video/Views.js
@@ -10,9 +10,19 @@ export const Div = styled.div.attrs({
   text-align: right;
 `;
 
+export const formatCount = (count) => {
+  const b = count / 1000000000;
+  const m = count / 1000000;
+  const k = count / 1000;
+  if (b > 1) return `${ Math.trunc(b * 10) / 10 }B`;
+  if (m > 1) return `${ Math.trunc(m * 10) / 10 }M`;
+  if (k > 1) return `${ Math.trunc(k * 10) / 10 }K`;
+  return `${count}`;
+};
+
 const Views = ({ count = 0 }) => (
   <Div>
-    { count > 0 ? count + ' views' : '' }
+    { count > 0 ? formatCount(count) + ' views' : '' }
   </Div>
 );
 Views.propTypes = {
--- src/ui/app/video/Views.test.js
+++ src/ui/app/video/Views.test.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import 'jest-styled-components';
 
-import Views, { Div } from './Views';
+import Views, { Div, formatCount } from './Views';
 
 describe('snapshots', () => {
+  it('formatCount', () => {
+    expect(formatCount(123)).toEqual('123');
+    expect(formatCount(1123)).toEqual('1.1K');
+    expect(formatCount(4231123)).toEqual('4.2M');
+    expect(formatCount(9834231123)).toEqual('9.8B');
+  });
   it('Div', () => {
     const wrapper = mount(
       <Div>children</Div>
